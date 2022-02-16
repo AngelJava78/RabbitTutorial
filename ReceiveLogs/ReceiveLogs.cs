8eecbc169e7bb5bcb7f2d1d5625cdbcddd314139
Angel Javier <angel_java@hotmail.com>
2022-02-15 18:15:19 -0600

    Publish/Subscribe were added to project

---
+++ ReceiveLogs/ReceiveLogs.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace ReceiveLogs
+{
+    public class ReceiveLogs
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Receive Logs program");
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare(exchange: "logs", ExchangeType.Fanout);
+
+                    //declare a server-named queue
+                    var queueName = channel.QueueDeclare(queue: "").QueueName;
+                    channel.QueueBind(queue: queueName, exchange: "logs", routingKey: "");
+
+                    Console.WriteLine(" [*] Waiting for logs.");
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body.ToArray();
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine($" [x] {message}");
+
+                    };
+                    channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+                    Console.WriteLine("Press any key to exit...");
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
